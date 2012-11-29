9d873315c4b810ecfefeff2ef5606bcdf420aeb3
Uday Kadaboina <uday88k@gmail.com>
2012-11-28 17:02:50 -0700

    added curriculum page

---
+++ curriculum.php
@@ -0,0 +1,44 @@
+
+<?php 
+include('header.php')
+
+?>
+    <div id="content_top">
+      <div class="content">
+        <ul id="unm_breadcrumbs">
+          <li class="unm_home"><a href="http://www.unm.edu/">UNM</a></li>
+          <li><span class="breadcrumb-div">»</span><a href="http://webmaster.unm.edu/index.html">Honors Home</a></li>
+        </ul>
+        
+      </div>
+    </div>
+ 
+    <div id="content">
+      <div class="content">
+        <div id="before_default_1"></div>
+        <div id="before_default_2"></div>
+        <div id="before_default_3"></div>
+        <div id="before_default_4"></div> 
+        <h1>Curriculum in Honors</h1>
+        <p>To successfully graduate with either an Honors College designation, minor or major, students must complete the requirements of the option they've chosen.  Link to criteria for these</p>
+        <p>Senior Exit Packets<br />(Available February 1 for Spring Graduates and October 1 for Fall Graduates)</p>
+        <p>Seniors are responsible for notifying the Honors College office of their intent to graduate.  They must complete a Senior Exit Packet at the beginning of their last semester at UNM. Packets are partially completed on-line with supplemental materials brought to the Honors College Office.&nbsp;</p>
+        <p>The packets have a crucial deadlines (please note the deadlines posted online and on posters throughout the honors center).The deadline is important in order to comply with the UNM Graduation Office, which ensures that seniors' names and distinctions in in the Honors College are included on UNM's main commencement College, as well as printed on official transcripts and diplomas.&nbsp;</p>
+        <p>Before you begin a Senior Review Form, <strong><a href="forms/Senior_Exit_Packet_Sample.pdf">WE STRONGLY RECOMMEND THAT YOU REVIEW THE SENIOR EXIT PACKET CHECKLIST AND SAMPLE SENIOR REVIEW FORM.</a></strong> Because you cannot save your progress on the Senior Review Form, have your responses ready before you begin. The Honors College office will not accept handwritten Senior Review Forms.</p>
+        <p>COMPLETE THE SENIOR REVIEW FORM HERE!! A copy of your completed Senior Review Form will be emailed to you upon completion.</p>
+        <h3>Exit Interviews</h3><p>An essential component of the Senior Exit Packet process is the Exit Interview. The Exit Interview is a unique conversation wherein students will be asked to assimilate their good and bad experiences in Honors and the University as a whole. This is conducted between a graduating senior and a continuing faculty member in the Honors College. Once the Senior Exit Packet has been completed, seniors will receive the name and contact information of the faculty member with whom they will interview. Seniors are responsible for contacting their interviewer and arranging the time, date, and location of this meeting. Prior to the interview, faculty members will review the senior's file along with all of the documents and evaluations from other Honors College instructors.</p>
+        <h3>Levels of Honors</h3><p>The University Honors Committee meets at the end of each semester to review the records of each graduating senior. Levels of Honors are awarded on the basis of GPA, success in Honors College courses, instructor evaluations, risks taken in Honors College courses, breadth of all academic course work, outside activities, and contributions to both the University and community. Levels of Honors are cum laude, magna cum laude, and summa cum laude.</p>
+        <h3>International Designation</h3><p>Students must have a minimum 3.5 cumulative grade point average and complete a minimum of 18 credit hours in both of the following areas with a grade of CR or better:<br />1.  Complete an international academic experience: Intersession, Summer or Semester College and earn a minimum of 6 credit hours; <br />2.  Complete a minimum of twelve (12) credit hours in a foreign language OR complete a minimum of (12) twelve credit hours in global/international coursework.&nbsp;</p>
+        <h3>Senior Recognition Ceremony</h3><p>At the end of the fall and spring semesters, the Honors College hosts a Senior Recognition Ceremony.  Senior will receive their certificate and medallion.  Families and friends are welcome.</p>
+        <div id="after_default_1"></div>
+        <div id="after_default_2"></div>
+        <div id="after_default_3"></div>
+        <div id="after_default_4"></div>
+      </div>
+    </div>
+
+
+<?php 
+include('socialtoolbar.php')
+
+?>
